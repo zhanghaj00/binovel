b6b139b0d09077e1c41ae4add75ec4e4caa20cec
zhanghaj00 <414080606@qq.com>
2014-07-22 20:21:51 +0800

    1

---
+++ src/main/java/com/binovel/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.binovel.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+	@RequestMapping("/login") 
+	public String UserLogin(){
+		return null;
+		
+	}
+}
